4fb5b7d4aadc109794985db6dd992272d7287b6b
elindblad <emil@lindblad.tech>
2020-09-22 11:16:51 +0200

    week4 ex1

--- week4/src/exercises/Ex1WordCount.java
+++ week4/src/exercises/Ex1WordCount.java
@@ -1,5 +1,6 @@
 package exercises;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static java.lang.System.in;
@@ -18,20 +19,32 @@ public class Ex1WordCount {
         new Ex1WordCount().program();
     }
 
+
     void program() {
-        //out.println(countWords("") == 0);
-        //out.println(countWords("hello") == 1);
-        //out.println(countWords(" hello ") == 1);
-        //out.println(countWords("hello world") == 2);
-        //out.println(countWords("hello        world") == 2);
-        //out.println(countWords("   hello        world  ") == 2);
+        out.println(countWords("") == 0);
+        out.println(countWords("hello") == 1);
+        out.println(countWords(" hello ") == 1);
+        out.println(countWords("hello world") == 2);
+        out.println(countWords("hello        world") == 2);
+        out.println(countWords("   hello        world  ") == 2);
         String s = "Education is what remains after one has forgotten what one has learned in school.";
-        //out.println(countWords(s) == 14);
+        out.println(countWords(s) == 14);
 
     }
 
     //--------------- Methods -----------------
-
-    // TODO
-
+    int countWords(String str) {
+        int words = 0;
+        str = str.trim();
+        if (str.isEmpty()) {
+           return words;
+        }
+        String[] wordlist = str.split(" ");
+        for (String word : wordlist) {
+            if (word.matches("^[^\\d\\s]+$")) {
+                words++;
+            }
+        }
+        return words;
+    }
 }
