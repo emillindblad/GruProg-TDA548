83721a12424e36bd4281d505a80930bbb7d6541d
elindblad <emil@lindblad.tech>
2020-09-24 14:04:40 +0200

    Ex2 week4

--- week4/src/exercises/Ex2PigLatin.java
+++ week4/src/exercises/Ex2PigLatin.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import java.util.Arrays;
+
 import static java.lang.System.out;
 
 /*
@@ -20,13 +22,41 @@ public class Ex2PigLatin {
     }
 
     void program() {
-      //  out.println(toPigLatin("My name is Eric")
-        //        .equals("yMay amenay isway Ericway"));
+        out.println(toPigLatin("My name is Eric").equals("yMay amenay isway Ericway"));
     }
 
     // ---------- Methods --------------------
 
-    // TODO
+    String toPigLatin(String str) {
+        String[] inputwords = str.split(" ");
+        StringBuilder word = new StringBuilder();
+        for (String inputword : inputwords) {
+            int firstletterType = checkFirstletter(inputword);
+            String encodedWord = encodeWord(inputword, firstletterType);
+            word.append(encodedWord).append(" ");
+        }
+        return word.toString().strip();
+    }
 
+    int checkFirstletter(String inputword) {
+        char firstLetter = inputword.charAt(0);
+        if (Character.toString(firstLetter).matches("[aoueiyAOUEIY]")) {
+            return 0; //First letter is vowel
+        } else {
+            return 1; //First letter is consonant
+        }
+    }
+
+    String encodeWord(String inputword, int type) {
+        StringBuilder encodedWord = new StringBuilder();
+        String vowelSuffix = "way";
+        String consonantSuffix = "ay";
+        if (type == 0) {
+            encodedWord.append(inputword).append(vowelSuffix);
+        } else {
+            encodedWord.append(inputword.substring(1)).append(inputword.charAt(0)).append(consonantSuffix);
+        }
+        return encodedWord.toString();
+    }
 
 }
